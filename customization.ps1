bcefba22eddf806731a6f479ae12b104354bd030
acalabrese09 <77075907+acalabrese09@users.noreply.github.com>
2021-09-28 09:11:50 -0400

    Update customization.ps1

--- customization.ps1
+++ customization.ps1
@@ -1,5 +1,5 @@
-﻿New-Item -Type Directory -Path 'c:\\' -Name temp1
-invoke-webrequest -uri 'https://ssd.mathworks.com/supportfiles/downloads/R2021a/Release/3/deployment_files/installer/complete/win64/MATLAB_Runtime_R2021a_Update_3_win64.zip' -OutFile 'c:\\temp1\\MATLAB_Runtime_R2021a_Update_3_win64.zip'
-Expand-Archive 'c:\\temp1\\MATLAB_Runtime_R2021a_Update_3_win64.zip' 'c:\\temp'
-cd C:\temp1
-./setup.exe -mode silent -agreeToLicense yes -outputFile ./install.log
+New-Item -Type Directory -Path 'c:\\' -Name temp1
+invoke-webrequest -Uri 'https://matlabruntime.blob.core.windows.net/mcr/MCR.zip?sp=rw&st=2021-09-28T13:10:59Z&se=2021-10-05T21:10:59Z&spr=https&sv=2020-08-04&sr=b&sig=szkSWuuRiLDjMqdV1hKiY8if5Ov1pi72GWOFKmJge%2BQ%3D' -OutFile 'c:\\temp1\\MATLAB_Runtime_R2021a_Update_3_win64.zip'
+Expand-Archive 'c:\\temp1\\MATLAB_Runtime_R2021a_Update_3_win64.zip' 'c:\\temp1'
+Set-Location C:\\temp1
+.//setup.exe -mode silent -agreeToLicense yes -outputFile .//install.log
