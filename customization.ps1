cb5837f1d5812aa89194b90e92c9e3cf7d0500ab
acalabrese09 <77075907+acalabrese09@users.noreply.github.com>
2021-09-28 09:09:37 -0400

    Add files via upload

---
+++ customization.ps1
@@ -0,0 +1,5 @@
+﻿New-Item -Type Directory -Path 'c:\\' -Name temp1
+invoke-webrequest -uri 'https://ssd.mathworks.com/supportfiles/downloads/R2021a/Release/3/deployment_files/installer/complete/win64/MATLAB_Runtime_R2021a_Update_3_win64.zip' -OutFile 'c:\\temp1\\MATLAB_Runtime_R2021a_Update_3_win64.zip'
+Expand-Archive 'c:\\temp1\\MATLAB_Runtime_R2021a_Update_3_win64.zip' 'c:\\temp'
+cd C:\temp1
+./setup.exe -mode silent -agreeToLicense yes -outputFile ./install.log
